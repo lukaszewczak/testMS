4056e2f6adaa14c2e00bd732a2cee431c3778110
Łukasz Szewczak <lukaszewczak@gmail.com>
2022-12-03 21:02:47 +0100

    fix spelling

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,7 +1,7 @@
 import { select, templates, settings, classNames } from '../settings.js';
 import utils from '../utils.js';
 import AmountWidget from './AmountWidget.js';
-import PeopleWidget from './peopleWidget.js';
+import PeopleWidget from './PeopleWidget.js';
 import DatePicker from './DatePicker.js';
 import HourPicker from './HourPicker.js';
 
